458d9479ee17d3c7c243ba577a5fd7fb84b34218
AbhayPatil <abhay.patil@students.iiit.ac.in>
2021-09-14 09:13:48 +0530

    updated file

--- include/functions.h
+++ include/functions.h
@@ -5,11 +5,14 @@ void ash_main();
 void ash_execute();
 void ash_clear();                   // Pending...
 void ash_cd();
+void ash_pwd();
+void ash_echo();
 
 void display();
 void get_home();
 void trim_spaces(char *str);
 int is_space(char c);
+int is_tab(char c);
 void get_command();
 
 #endif
