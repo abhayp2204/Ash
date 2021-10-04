7b46ee4f61d521fb99d0ee3407e3852b30d64e5d
AbhayPatil <abhay.patil@students.iiit.ac.in>
2021-10-04 15:49:19 +0530

    updated file

--- src/ash_dump.c
+++ src/ash_dump.c
@@ -1,112 +1,41 @@
-// #ifndef SHELL_H
-// #include"../include/Shell.h"
-// #endif
-// #ifndef VARIABLE_H
-// #include"../include/Variables.h"
-// #endif
-// #ifndef FUNC_H
-// #include"../include/Functions.h"
-// #endif
+// #include "../include/shell.h"
+// #include "../include/functions.h"
+// #include "../include/variables.h"
 
-// void ash_pipe()
+// handler* intall_signal(int signal_no, handler* handler);
+
+// void handler(int signal)
 // {
-// 	// Variables for tokenization
-// 	char *token;
-// 	char *dup_in = (char*)malloc(MAX_COMM*sizeof(char));
-// 	strcpy(dup_in, read_in);
+//     // Ignore termination of foreground processes
+//     if(!flag_bg)
+//         return;
 
-// 	// We extract tokens by space
-// 	token = strtok(dup_in, " ");
-// 	if(!strcmp(token, "|"))
+//     char exit_message[1024];
+//     int status;
+//     pid_t pid;
+
+// 	while((pid = waitpid(-1, &status, WNOHANG)) > 0)
 // 	{
-// 		write(2, "ash: pipe: Pipe cannot read from NULL command", strlen("ash: pipe: Pipe cannot read from NULL command"));
-// 		newlerr();
-// 		suc_flag = 1;
-// 		return;
-// 	}
-
-// 	int pipe_count = 0;
-
-// 	// Count the number of pipes
-// 	token = strtok(NULL, " ");
-// 	while(token != NULL)
-// 	{
-// 		if(!strcmp(token, "|"))
+// 		for(int i = 0; i < MAX_BG_PROCESSES ; i++)
 // 		{
-// 			pipe_count++;
-// 			token = strtok(NULL, " ");
-// 			if(token == NULL)
+// 			if(child_process[i].pid == pid)
 // 			{
-// 				write(2, "ash: pipe: Pipe cannot write to NULL command", strlen("ash: pipe: Pipe cannot write to NULL command"));
-// 				newlerr();
-// 				suc_flag = 1;
-// 				return;
+// 				sprintf(exit_message, "\n%s with pid %d exited %s\n", child_process[i].name, pid, WIFEXITED(status) == 0 ? "abnormally" : "normally");
+// 				wprint(exit_message);
+                
+// 				pop_child(i);
+// 				break;
 // 			}
 // 		}
-
-// 		token = strtok(NULL, " ");
 // 	}
 
-// 	// If there's no piping, return to normal execution
-// 	if(!pipe_count)
-// 		return;
+//     display_banner();
+// }
 
-// 	strcpy(dup_in, read_in);
-// 	token = strtok(dup_in, "|");
-
-
-// 	int new_in = STDIN_FILENO;
-// 	pid_t pid;
-
-// 	// For every | character, make a pipe. The first command reads from stdin, and the last command writes to stdout
-// 	// The rest of the commands read from the read end of the previous pipe, and write to the write end of their own pipe.
-// 	// Redirection takes precedence over pipes, emulating bash behavior. 
-// 	for(int i = 0; i<=pipe_count; i++)
-// 	{
-// 		int pipes[2];
-// 		pipe(pipes);
-
-// 		strcpy(read_in, token);
-// 		clean_string(read_in);
-// 		token = strtok(NULL, "|");
-
-// 		pid = fork();
-
-// 		// Error in forking
-// 		if(pid < 0)
-// 		{
-// 			write(2, "ash: Failed to spawn new process", strlen("ash: Failed to spawn new process"));
-// 			newlerr();
-// 			suc_flag = 1;
-// 			return;
-// 		}
-
-// 		// Child process runs the command, and creates the pipe
-// 		if(pid == 0)
-// 		{
-// 			dup2(new_in, 0);
-
-// 			if(i != pipe_count)
-// 				dup2(pipes[1], 1);
-
-// 			close(pipes[0]);
-// 			ash_redir();
-// 			if(strlen(read_in))
-// 				ash_builtin();
-// 			exit(0);
-// 		}
-
-// 		// Parent waits for termination of the child, and closes the unused write end of the pipe
-// 		// Also sets the read end of the pipe for the next command
-// 		else
-// 		{
-// 			waitpid(pid, NULL, 0);
-// 			close(pipes[1]);
-// 			new_in = pipes[0];
-// 		}
-// 	}
-
-// 	// Make the command null, and free the copied variable
-// 	read_in[0] = '\0';
-// 	free(dup_in);
+// void pop_child(int i)
+// {
+// 	child_process[i].pid = NOT_CREATED;
+// 	child_process[i].pos = NOT_CREATED;
+// 	child_process[i].name[0] = '\0';
+// 	number_of_children--;
 // }
